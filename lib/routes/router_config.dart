a2a5269cdb6dd7709df26b0ec1aa574be04018a7
colorman-985 <phamxuanbac28102003@gmail.com>
2026-06-29 21:25:13 +0700

    [UI] Onboarding

--- lib/routes/router_config.dart
+++ lib/routes/router_config.dart
@@ -4,6 +4,11 @@ import 'package:duantotnghiep_app_thue_xe/views/setting_view.dart';
 import 'package:duantotnghiep_app_thue_xe/views/order_detail.dart';
 import 'package:duantotnghiep_app_thue_xe/views/car_detail.dart';
 import 'package:duantotnghiep_app_thue_xe/views/profile.dart';
+import 'package:duantotnghiep_app_thue_xe/views/onboardings/slide1_view.dart';
+import 'package:duantotnghiep_app_thue_xe/views/onboardings/slide2_view.dart';
+import 'package:duantotnghiep_app_thue_xe/views/onboardings/slide3_view.dart';
+import 'package:duantotnghiep_app_thue_xe/views/onboardings/slide4_view.dart';
+
 import 'package:go_router/go_router.dart';
 
 // GoRouter configuration
@@ -25,5 +30,13 @@ final drivioRouter = GoRouter(
       builder: (context, state) => const CarDetailPage(),
     ),
     GoRoute(path: '/profile', builder: (context, state) => const ProfilePage()),
+    GoRoute(
+      path: '/order-detail',
+      builder: (context, state) => const OrderDetailView(),
+    ),
+    GoRoute(path: '/slide1', builder: (context, state) => const Slide1View()),
+    GoRoute(path: '/slide2', builder: (context, state) => const Slide2View()),
+    GoRoute(path: '/slide3', builder: (context, state) => const Slide3View()),
+    GoRoute(path: '/slide4', builder: (context, state) => const Slide4View()),
   ],
 );
