16abce4c1f0866f12f838d6bdc786391fc2ef53f
Huy Ben <benpham06@gmail.com>
2026-06-29 19:24:35 +0700

    [UI] Car_Detail

--- lib/routes/router_config.dart
+++ lib/routes/router_config.dart
@@ -1,6 +1,7 @@
 import 'package:duantotnghiep_app_thue_xe/views/demo_page.dart';
 import 'package:duantotnghiep_app_thue_xe/views/home_view.dart';
 import 'package:duantotnghiep_app_thue_xe/views/setting_view.dart';
+import 'package:duantotnghiep_app_thue_xe/views/car_detail.dart';
 import 'package:go_router/go_router.dart';
 
 // GoRouter configuration
@@ -9,5 +10,6 @@ final drivioRouter = GoRouter(
     GoRoute(path: '/', builder: (context, state) => const HomeView()),
     GoRoute(path: '/setting', builder: (context, state) => const SettingView()),
     GoRoute(path: '/demo', builder: (context, state) => const DemoPage()),
+    GoRoute(path: '/car_detail', builder: (context, state) => const CarDetailPage())
   ],
 );
