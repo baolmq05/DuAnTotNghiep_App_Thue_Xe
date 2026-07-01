319049c1e7f52605834f92b4a294a37d98d94af3
hoangbao <rowillius@gmail.com>
2026-07-01 19:39:33 +0700

    [UI] Chat

--- lib/routes/router_config.dart
+++ lib/routes/router_config.dart
@@ -1,13 +1,25 @@
 import 'package:duantotnghiep_app_thue_xe/views/demo_page.dart';
 import 'package:duantotnghiep_app_thue_xe/views/home_view.dart';
 import 'package:duantotnghiep_app_thue_xe/views/setting_view.dart';
+import 'package:duantotnghiep_app_thue_xe/views/message/message_list_view.dart';
+import 'package:duantotnghiep_app_thue_xe/views/message/chat_detail_view.dart';
+import 'package:duantotnghiep_app_thue_xe/models/message_model.dart';
 import 'package:go_router/go_router.dart';
 
 // GoRouter configuration
 final drivioRouter = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (context, state) => const HomeView()),
+    GoRoute(path: '/chat', builder: (context, state) => const MessageListView()),
     GoRoute(path: '/setting', builder: (context, state) => const SettingView()),
     GoRoute(path: '/demo', builder: (context, state) => const DemoPage()),
+    GoRoute(
+      path: '/chat/:id',
+      builder: (context, state) {
+        final id = state.pathParameters['id']!;
+        final conv = state.extra as Conversation?;
+        return ChatDetailView(conversationId: id, conversation: conv);
+      },
+    ),
   ],
 );
